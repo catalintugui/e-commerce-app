6e5a5389ce2de6ce58f95a2e0e861b85a695089c
Catalin Tugui <katamaxx23@gmail.com>
2022-06-03 16:02:18 +0300

    initial files + initial UI

---
+++ crwn-app/src/App.js
@@ -0,0 +1,9 @@
+import CategoryContainer from "./components/categories-container/categories-container.component";
+
+const App = () => { 
+  return (
+    <CategoryContainer />
+  );
+};
+
+export default App;
