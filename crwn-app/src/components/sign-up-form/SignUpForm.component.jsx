a04f2bbf79748ed6cfa9ce1fcb33c0d63a8ec3a5
Catalin Tugui <katamaxx23@gmail.com>
2022-11-03 22:14:16 +0200

    sign up component

--- crwn-app/src/components/sign-up-form/SignUpForm.component.jsx
+++ crwn-app/src/components/sign-up-form/SignUpForm.component.jsx
@@ -1,8 +1,12 @@
 import { useState } from "react";
+import FormInput from "../form-input/form-input.component";
 import {
   createAuthUserWithEmailAndPassword,
   createUserDocumentFromAuth,
 } from "../../utils/firebase/firebase.utils";
+import Button from "../button/button.component";
+
+import "./sign-up-form.styles.scss";
 
 const defaultFormFields = {
   displayName: "",
@@ -49,42 +53,43 @@ const SignUpForm = () => {
   };
 
   return (
-    <div>
-      <h1>Sign up with your email and password</h1>
+    <div className="sign-up-container">
+      <h2>Don't have an account?</h2>
+      <span>Sign up with your email and password</span>
       <form onSubmit={handleSubmit}>
-        <label>Displayed name</label>
-        <input
+        <FormInput
+          label="Display Name"
           type="text"
           name="displayName"
           required
           value={displayName}
           onChange={handleChange}
         />
-        <label>Email</label>
-        <input
+        <FormInput
+          label="Email"
           type="email"
           name="email"
           required
           value={email}
           onChange={handleChange}
         />
-        <label>Password</label>
-        <input
+        <FormInput
+          label="Password"
           type="password"
           name="password"
           required
           value={password}
           onChange={handleChange}
         />
-        <label>Confirm password</label>
-        <input
+        <FormInput
+          label="Confirm password"
           type="password"
           name="confirmPassword"
           required
           value={confirmPassword}
           onChange={handleChange}
         />
-        <button type="submit">Sign up</button>
+        <Button type="submit">Sign up</Button>
       </form>
     </div>
   );
